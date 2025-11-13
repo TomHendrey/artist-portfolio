a459a931f8146690731f7727a42c97e2e23da4e3
Thomas Hendrey <thomashendrey@mac.home>
2025-11-13 10:02:09 +0000

    Edited text

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -108,7 +108,7 @@ export default function About() {
                             className="text-neutral-500 text-base italic"
                             style={{ fontFamily: "Cardo, serif" }}
                         >
-                            - Alex Kendal White 2025
+                            - Kendall White 2025
                         </p>
                     </div>
                 </div>
